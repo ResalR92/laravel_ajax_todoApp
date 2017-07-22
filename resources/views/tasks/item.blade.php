8e87d632e289e0929f0dd084cfc1e8d4badfbad1
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-07-23 00:03:11 +0700

    FIX->error->data-url->$task->todoList->id->menjadi->$task->todo_list_id

--- resources/views/tasks/item.blade.php
+++ resources/views/tasks/item.blade.php
@@ -1,12 +1,12 @@
 <tr class="task-item" id="task-{{ $task->id }}">
     <td>
-        <input type="checkbox" {{-- data-url="{{ route('todolists.tasks.update',[$task->todoList->id, $task->id]) }}" --}} {{ !$task->completed ?: "checked=true" }} class="check-item">
+        <input type="checkbox" data-url="{{ route('todolists.tasks.update',[$task->todo_list_id, $task->id]) }}" {{ !$task->completed ?: "checked=true" }} class="check-item">
     </td>
     <td  class="task-item-title {{ !$task->completed ?: 'done' }}">
         {{ $task->title }}
         <div class="row-buttons">
-            <a href="{{-- {{ route('todolists.tasks.destroy',[$task->todoList->id, $task->id]) }} --}}" class="btn btn-xs btn-danger remove-task-btn">
-                <i class="glyphicon glyphicon-remove"></i>
+            <a href="{{ route('todolists.tasks.destroy',[$task->todo_list_id, $task->id]) }}" class="btn btn-xs btn-danger remove-task-btn">
+                <i class="glyphicon glyphicon-remove"></i> 
             </a>
         </div>
     </td>
